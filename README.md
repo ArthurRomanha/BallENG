02607edaa0bb2a12fcf5b29105b7f0ab428f43b0
Arthur Romanha <126591597+ArthurRomanha@users.noreply.github.com>
2024-08-09 00:01:20 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # BolidENG
+Projeto desenvolvido com HTML, CSS, JavaScript e JSON para exercitar meus aprendizados
--- script.js
+++ script.js
@@ -1,11 +1,45 @@
-let divVictory = document.querySelector(".victory");
-const winGame=()=>{
-    fetch("dados.JSON").then((response)=>{
-        response.json().then((object)=>{
-            object.words.map((words)=>{
+const divVictory = document.querySelector(".victory");
+const divScreens = document.querySelector(".screens");
+const secondScreen = document.querySelectorAll(".secondScreen");
+const selectDifficult = document.querySelectorAll("#selectDifficult");
+const instructionsDescription = document.querySelector("#instructionsDescription");
+const btnContinue = document.querySelector("#continue");
+
+let screen = 1;
+
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');;
+
+const nextScreen = () => {
+    switch (screen) {
+        case 1:
+            secondScreen.forEach(element => {
+                element.style.visibility = "visible";
+            })
+            screen++;
+            break;
+        case 2:
+            instructionsDescription.style.visibility = "hidden";
+            btnContinue.style.visibility = "hidden"
+            selectDifficult.forEach(element => {
+                element.style.visibility = "visible";
+            })            
+            break;
+    }
+}
+
+const winGame = () => {
+    fetch("dados.JSON").then((response) => {
+        response.json().then((object) => {
+            object.words.map((words) => {
                 divVictory.innerHTML += `<h1>${words.name}</h1>
                                         <h2>${words.mean}</h2>`;
             })
         })
     })
+}
+const game = () => {
+    divScreens.style.display = "none";
+    canvas.style.display = "block";
+    
 }
